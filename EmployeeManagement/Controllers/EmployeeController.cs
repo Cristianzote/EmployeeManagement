c1723507ae6d1cf20b09f8e4427602d4c00db50f
Cristianzote <125917044+Cristianzote@users.noreply.github.com>
2024-08-29 15:34:17 -0500

    Init

---
+++ EmployeeManagement/Controllers/EmployeeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeDbContext db = new EmployeeDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            var employees = db.Employees.ToList();
+            return Json(employees);
+        }
+
+        [HttpPost]
+        public JsonResult UpdateEmployee(EmployeeController employee)
+        {
+            db.Entry(employee).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(employee);
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ EmployeeManagement/EmployeeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public EmployeeDbContext() : base("SomeeSqlDbConnection")
+        {
+        }
+    }
+}
